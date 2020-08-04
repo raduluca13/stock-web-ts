25a43d588a255b461eecf0a868656136e32a1d24
Radu Luca <radu.luca13@yahoo.com>
2020-08-04 15:23:50 +0300

    try handbook typescript react guide

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// eslint-disable-next-line
+import * as serviceWorker from "./serviceWorker";
+import { createStore, Action } from "redux";
+import { enthusiasm } from "./screens/app/state/reducers/index";
+import { StoreState } from "./screens/app/state/types/index";
+import Hello from "./screens/app/containers/Hello";
+import { Provider } from "react-redux";
+
+const store = createStore<StoreState, Action<any>, any, any>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: "TypeScript",
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
