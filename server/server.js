23a1be87312fe6ecbbca057692b6eb7c54dcb981
Radu Luca <radu.luca@cegeka.com>
2020-08-31 17:36:35 +0300

    express changes

--- server/server.js
+++ server/server.js
@@ -2,11 +2,9 @@ const path = require("path");
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "/", "public");
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
